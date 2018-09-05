e7cdfa3c57a89c2ca0a188da98c4e55907758576
Unknown <tristan.m.orourke@gmail.com>
2018-09-05 18:20:16 -0400

    Commenting out utility classes, moving debugbar to required composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,14 +28,14 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
 
-        if ($this->app->environment() == 'local') {
-            //Generate migrations from database
-            $this->app->register(\Way\Generators\GeneratorsServiceProvider::class);
-            $this->app->register(\Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
-
-            //Generate models from database
-            $this->app->register(\Reliese\Coders\CodersServiceProvider::class);
-        }
+        // if ($this->app->environment() == 'local') {
+        //     //Generate migrations from database
+        //     $this->app->register(\Way\Generators\GeneratorsServiceProvider::class);
+        //     $this->app->register(\Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
+        //
+        //     //Generate models from database
+        //     $this->app->register(\Reliese\Coders\CodersServiceProvider::class);
+        // }
 
         $this->app->singleton(WhichPortal::class, function ($app) {
             return new WhichPortal();
